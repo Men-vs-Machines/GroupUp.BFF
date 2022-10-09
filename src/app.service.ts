4a52cf8a1d363679818d1b979c04b0395e134a05
henry <hnr.leto11@gmail.com>
2022-10-09 11:38:56 -0400

    init firebase setup

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import * as admin from 'firebase-admin';
+import { firestore } from 'firebase-admin';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly db: firestore.Firestore;
+
+  constructor() {
+    // this.db = admin.firestore();
+  }
+
+  async getHello() {
+    return 'hello';
+    // const groupRef = this.db.collection('Groups').doc('9F34J3byex4l7ZSCixgH');
+    // const group = await groupRef.get();
+    // if (group.exists) {
+    //   return group.data();
+    // }
   }
 }
